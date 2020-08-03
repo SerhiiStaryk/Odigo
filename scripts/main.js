5c24a2d2fe908206f5113506cf808efef5fd26e2
Serhii Staryk <serhii.staryk@gmail.com>
2020-08-03 11:34:22 +0300

    finish 7 lesson

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,25 @@
 (function () {
     const header = document.querySelector('.header');
 
-    console.log(header);
-
     window.onscroll = () => {
-        if(window.pageYOffset > 50) {
+        if (window.pageYOffset > 50) {
             header.classList.add('header__active');
         } else {
             header.classList.remove('header__active');
         }
     }
+}());
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
 }());
